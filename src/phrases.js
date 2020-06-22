454e98efcce9d976be12dfd4ea2609c9dea55759
chmelan <chmelan@comcast.net>
2020-06-22 12:48:31 -0400

    added basic placeholder phrases for testing

--- src/phrases.js
+++ src/phrases.js
@@ -5,7 +5,23 @@ const phrases = [
   },
   {
     phrase: 'For honor!',
+    tags: ['dwarf', 'warrior', 'paladin', 'human'],
+  },
+  {
+    phrase: 'Your mother was a orc',
     tags: ['dwarf', 'warrior', 'paladin'],
+  },
+  {
+    phrase: 'May the trees guide you',
+    tags: ['elf', 'ranger', 'druid'],
+  },
+  {
+    phrase: "Time to steal anything that isn't nailed down!",
+    tags: ['rouge', 'halfling', 'human'],
+  },
+  {
+    phrase: 'What ancient secrets do you contain?',
+    tags: ['wizard', 'elf', 'human'],
   },
 ];
 
