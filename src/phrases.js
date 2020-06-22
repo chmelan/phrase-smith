08f1f135bd69566028c7ddca827f0c45efc34d83
chmelan <chmelan@comcast.net>
2020-06-22 12:40:59 -0400

    more initial setup

---
+++ src/phrases.js
@@ -0,0 +1,12 @@
+const phrases = [
+  {
+    phrase: 'By my beard!',
+    tags: ['dwarf', 'warrior', 'barbarian'],
+  },
+  {
+    phrase: 'For honor!',
+    tags: ['dwarf', 'warrior', 'paladin'],
+  },
+];
+
+export default phrases;
